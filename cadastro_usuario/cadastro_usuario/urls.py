4bd7de070ada6305240148196d7d72e1408a9b4a
weslyramalho <weslyramalho@yahoo.com.br>
2023-09-22 18:31:10 -0300

    usuarios list

--- cadastro_usuario/cadastro_usuario/urls.py
+++ cadastro_usuario/cadastro_usuario/urls.py
@@ -5,4 +5,5 @@ from app_cad_usuario import views
 urlpatterns = [
     # rota, view responsavel, nome de referencia
     path('', views.home,name='home'),
+    path('usuarios/', views.usuarios, name='listagem_usuarios')
 ]
