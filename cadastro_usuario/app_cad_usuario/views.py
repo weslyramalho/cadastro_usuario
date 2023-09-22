be39785f8e433fe294be5fbdcab56608d3a32ab5
weslyramalho <weslyramalho@yahoo.com.br>
2023-09-22 18:59:11 -0300

    Exibindo dados salvo

--- cadastro_usuario/app_cad_usuario/views.py
+++ cadastro_usuario/app_cad_usuario/views.py
@@ -5,7 +5,15 @@ def home(request):
     return render(request, 'usuarios/home.html')
 
 def usuarios(request):
+    #salvando no banco de dados
     novo_usuario = Usuario()
     novo_usuario.nome= request.POST.get('nome')
     novo_usuario.idade = request.POST.get('idade')
     novo_usuario.save()
+
+    #Exibindo dados em nova pagina
+    usuarios = {
+        'usuarios': Usuario.objects.all()
+    }
+
+    return render(request, 'usuarios/usuarios.html', usuarios)
